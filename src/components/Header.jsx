9eea6454ea8297272b645c68a94c5f248fc5faf0
priyanshyv <priyansh.yv7@gmail.com>
2025-04-15 00:27:40 +0530

    green dot

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,14 +13,26 @@ const Header = () => {
           revealDirection="center"
         />
     
+          <SiRetroarch color="white" className="ml-50"/>
           
-          <SiRetroarch color="white" className="ml-50"/></h1>
+        </h1>
         <p className="flex items-center text-sm ">
         <CiLocationOn className="mr-1 " />
         <span className="text-slate-200">Jaipur, India</span>
       </p>
         <p className=" text-xl text-gray-400 font-retro">I'm 20-year-old Computer Science undergrad. My passion lies in building innovative solutions and tackling challenging problems. I thrive on the intersection computer science and practical application, often finding myself immersed in coding while enjoying Lofi-music. I'm always eager to learn and grow in this ever-evolving tech landscape. Whether it’s designing clean UIs or tackling backend logic, I’m always excited to learn, build, and push my limits.</p>
         <p className="text-xl text-gray-400 font-retro">In the past I’ve developed the <a href="https://youtu.be/AROVBbZM3A0" className="underline hover:text-white transition-colors" target="_blank">Reposheet</a>,  <a href="https://padhaicrow.vercel.app/" className="underline hover:text-white transition-colors" target="_blank">StudyNotion</a> and some small but good projects.</p>
+        <p className="flex items-center text-green-400 text-sm">
+                    <a
+              href="https://www.linkedin.com/in/your-profile/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center text-yellow-400 text-sm hover:underline"
+            >
+              <span className="h-3 w-3 rounded-full bg-green-500 mr-2 animate-pulse" />
+              <span className="text-slate-200">Available for new opportunities</span>
+            </a>
+        </p>
       </header>
     );
   };
